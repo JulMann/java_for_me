55bb4374eaa057d9bf2844109fe6f0d765eae802
JullMann <julia.mannanova@gmail.com>
2024-07-30 18:16:14 +0300

    Переделан базовый класс для тестов TestBase

--- addressbook_web_testers/src/test/java/ContactCreationTests.java
+++ addressbook_web_testers/src/test/java/ContactCreationTests.java
@@ -1,101 +1,15 @@
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 public class ContactCreationTests extends TestBase {
 
     @Test
     public void canCreateContactWithAllField() {
-        driver.findElement(By.linkText("add new")).click();
-        // Заполняем фио
-        driver.findElement(By.name("firstname")).click();
-        driver.findElement(By.name("firstname")).sendKeys("firstname");
-        driver.findElement(By.name("middlename")).click();
-        driver.findElement(By.name("middlename")).sendKeys("middlename");
-        driver.findElement(By.name("lastname")).click();
-        driver.findElement(By.name("lastname")).sendKeys("lastname");
-        driver.findElement(By.name("nickname")).click();
-        driver.findElement(By.name("nickname")).sendKeys("nickname");
-        // Заполняем e-mail
-        driver.findElement(By.name("email")).click();
-        driver.findElement(By.name("email")).sendKeys("email");
-        driver.findElement(By.name("email2")).click();
-        driver.findElement(By.name("email2")).sendKeys("email2");
-        driver.findElement(By.name("email3")).click();
-        driver.findElement(By.name("email3")).sendKeys("email3");
-        // Заполняем Адрес
-        driver.findElement(By.name("address")).click();
-        driver.findElement(By.name("address")).sendKeys("address");
-        // Заполняем Телефоны
-        driver.findElement(By.name("home")).click();
-        driver.findElement(By.name("home")).sendKeys("home_tel");
-        driver.findElement(By.name("mobile")).click();
-        driver.findElement(By.name("mobile")).sendKeys("mobile_tel");
-        driver.findElement(By.name("work")).click();
-        driver.findElement(By.name("work")).sendKeys("work_tel");
-        driver.findElement(By.name("fax")).click();
-        driver.findElement(By.name("fax")).sendKeys("fax");
-        // Заполняем День рождения
-        driver.findElement(By.name("bday")).click();
-        {
-            WebElement dropdown = driver.findElement(By.name("bday"));
-            dropdown.findElement(By.xpath("//option[. = '1']")).click();
-        }
-        driver.findElement(By.name("bmonth")).click();
-        {
-            WebElement dropdown = driver.findElement(By.name("bmonth"));
-            dropdown.findElement(By.xpath("//option[. = 'January']")).click();
-        }
-        driver.findElement(By.name("byear")).click();
-        driver.findElement(By.name("byear")).sendKeys("2001");
-        driver.findElement(By.xpath("(//input[@name=\'submit\'])[2]")).click();
+        createContactWithAllField();
     }
 
     @Test
-    public void canCreateContactWithSomeEmptyField() {
-        driver.findElement(By.linkText("add new")).click();
-        // Заполняем только Ник
-        driver.findElement(By.name("firstname")).click();
-        driver.findElement(By.name("firstname")).sendKeys("");
-        driver.findElement(By.name("middlename")).click();
-        driver.findElement(By.name("middlename")).sendKeys("");
-        driver.findElement(By.name("lastname")).click();
-        driver.findElement(By.name("lastname")).sendKeys("");
-        driver.findElement(By.name("nickname")).click();
-        driver.findElement(By.name("nickname")).sendKeys("nickname");
-        // Заполняем e-mail
-        driver.findElement(By.name("email")).click();
-        driver.findElement(By.name("email")).sendKeys("email");
-        driver.findElement(By.name("email2")).click();
-        driver.findElement(By.name("email2")).sendKeys("");
-        driver.findElement(By.name("email3")).click();
-        driver.findElement(By.name("email3")).sendKeys("");
-        // Заполняем Адрес
-        driver.findElement(By.name("address")).click();
-        driver.findElement(By.name("address")).sendKeys("address");
-        // Заполняем Телефоны
-        driver.findElement(By.name("home")).click();
-        driver.findElement(By.name("home")).sendKeys("");
-        driver.findElement(By.name("mobile")).click();
-        driver.findElement(By.name("mobile")).sendKeys("mobile_tel");
-        driver.findElement(By.name("work")).click();
-        driver.findElement(By.name("work")).sendKeys("");
-        driver.findElement(By.name("fax")).click();
-        driver.findElement(By.name("fax")).sendKeys("");
-        // Заполняем День рождения
-        driver.findElement(By.name("bday")).click();
-        {
-            WebElement dropdown = driver.findElement(By.name("bday"));
-            dropdown.findElement(By.xpath("//option[. = '']")).click();
-        }
-        driver.findElement(By.name("bmonth")).click();
-        {
-            WebElement dropdown = driver.findElement(By.name("bmonth"));
-            dropdown.findElement(By.xpath("//option[. = '']")).click();
-        }
-        driver.findElement(By.name("byear")).click();
-        driver.findElement(By.name("byear")).sendKeys("");
-        driver.findElement(By.xpath("(//input[@name=\'submit\'])[2]")).click();
+    public void canCreateContactWithAllEmptyField() {
+        createContactWithEmtyField();
     }
 
 }
--- addressbook_web_testers/src/test/java/TestBase.java
+++ addressbook_web_testers/src/test/java/TestBase.java
@@ -5,6 +5,104 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TestBase {
     protected static WebDriver driver;
 
+    protected static void createContactWithAllField() {
+        driver.findElement(By.linkText("add new")).click();
+        // Заполняем фио
+        driver.findElement(By.name("firstname")).click();
+        driver.findElement(By.name("firstname")).sendKeys("firstname");
+        driver.findElement(By.name("middlename")).click();
+        driver.findElement(By.name("middlename")).sendKeys("middlename");
+        driver.findElement(By.name("lastname")).click();
+        driver.findElement(By.name("lastname")).sendKeys("lastname");
+        driver.findElement(By.name("nickname")).click();
+        driver.findElement(By.name("nickname")).sendKeys("nickname");
+        // Заполняем e-mail
+        driver.findElement(By.name("email")).click();
+        driver.findElement(By.name("email")).sendKeys("email");
+        driver.findElement(By.name("email2")).click();
+        driver.findElement(By.name("email2")).sendKeys("email2");
+        driver.findElement(By.name("email3")).click();
+        driver.findElement(By.name("email3")).sendKeys("email3");
+        // Заполняем Адрес
+        driver.findElement(By.name("address")).click();
+        driver.findElement(By.name("address")).sendKeys("address");
+        // Заполняем Телефоны
+        driver.findElement(By.name("home")).click();
+        driver.findElement(By.name("home")).sendKeys("home_tel");
+        driver.findElement(By.name("mobile")).click();
+        driver.findElement(By.name("mobile")).sendKeys("mobile_tel");
+        driver.findElement(By.name("work")).click();
+        driver.findElement(By.name("work")).sendKeys("work_tel");
+        driver.findElement(By.name("fax")).click();
+        driver.findElement(By.name("fax")).sendKeys("fax");
+        // Заполняем День рождения
+        driver.findElement(By.name("bday")).click();
+        {
+            WebElement dropdown = driver.findElement(By.name("bday"));
+            dropdown.findElement(By.xpath("//option[. = '1']")).click();
+        }
+        driver.findElement(By.name("bmonth")).click();
+        {
+            WebElement dropdown = driver.findElement(By.name("bmonth"));
+            dropdown.findElement(By.xpath("//option[. = 'January']")).click();
+        }
+        driver.findElement(By.name("byear")).click();
+        driver.findElement(By.name("byear")).sendKeys("2001");
+        driver.findElement(By.xpath("(//input[@name=\'submit\'])[2]")).click();
+    }
+
+    protected static void createContactWithEmtyField() {
+        driver.findElement(By.linkText("add new")).click();
+        // Оставляем все поля пустыми
+        driver.findElement(By.name("firstname")).click();
+        driver.findElement(By.name("firstname")).sendKeys("");
+        driver.findElement(By.name("middlename")).click();
+        driver.findElement(By.name("middlename")).sendKeys("");
+        driver.findElement(By.name("lastname")).click();
+        driver.findElement(By.name("lastname")).sendKeys("");
+        driver.findElement(By.name("nickname")).click();
+        driver.findElement(By.name("nickname")).sendKeys("");
+
+        driver.findElement(By.name("email")).click();
+        driver.findElement(By.name("email")).sendKeys("");
+        driver.findElement(By.name("email2")).click();
+        driver.findElement(By.name("email2")).sendKeys("");
+        driver.findElement(By.name("email3")).click();
+        driver.findElement(By.name("email3")).sendKeys("");
+
+        driver.findElement(By.name("address")).click();
+        driver.findElement(By.name("address")).sendKeys("");
+
+        driver.findElement(By.name("home")).click();
+        driver.findElement(By.name("home")).sendKeys("");
+        driver.findElement(By.name("mobile")).click();
+        driver.findElement(By.name("mobile")).sendKeys("");
+        driver.findElement(By.name("work")).click();
+        driver.findElement(By.name("work")).sendKeys("");
+        driver.findElement(By.name("fax")).click();
+        driver.findElement(By.name("fax")).sendKeys("");
+
+        driver.findElement(By.name("bday")).click();
+        {
+            WebElement dropdown = driver.findElement(By.name("bday"));
+            dropdown.findElement(By.xpath("//option[. = '']")).click();
+        }
+        driver.findElement(By.name("bmonth")).click();
+        {
+            WebElement dropdown = driver.findElement(By.name("bmonth"));
+            dropdown.findElement(By.xpath("//option[. = '']")).click();
+        }
+        driver.findElement(By.name("byear")).click();
+        driver.findElement(By.name("byear")).sendKeys("");
+        driver.findElement(By.xpath("(//input[@name=\'submit\'])[2]")).click();
+    }
+
+    protected static void removalContact() {
+        driver.findElement(By.linkText("home")).click();
+        driver.findElement(By.xpath("//table[@id=\'maintable\']/tbody/tr[2]/td/input")).click();
+        driver.findElement(By.xpath("//input[@value=\'Delete\']")).click();
+    }
+
     @BeforeEach
     public void setUp() {
         if (driver == null) {
